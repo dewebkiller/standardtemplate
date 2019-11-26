07db5ded6af6b2d3243d49679e0ba952336d60e9
Raaz shrestha <37860912+dewebkiller@users.noreply.github.com>
2019-11-26 15:06:43 +0545

    create index.php

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php include_once('header.php'); ?>
+<section class="banner-wrapper">
+    
+    <div class="owl-carousel owl-theme">
+        
+        <div class="item">
+            <div class="banneroverlay"></div>
+            <img src="images/banner1.jpg">
+            <div class="banner-caption">
+                <h2>The easiest way to book reliable household help</h2>
+                <p>Friendly, experienced cleaners available on your schedule. <br>Just pick a time and we'll do the rest.</p>
+                <a href="#" class="dwk-btn hvr-float">Read More</a>
+            </div>
+        </div>
+        <div class="item">
+            <div class="banneroverlay"></div>
+            <img src="images/banner2.jpg">
+            <div class="banner-caption">
+                <h2>The easiest way to book reliable household help</h2>
+                <p>Friendly, experienced cleaners available on your schedule. <br>Just pick a time and we'll do the rest.</p>
+                <a href="#" class="dwk-btn hvr-float">Read More</a>
+            </div>
+        </div>
+        
+    </div>
+</section>
+<section class="content-wrapper section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-padding quote-block">
+    <div class="container">
+        <div class="row wow animate slideInUp">
+            <div class="col-lg-8">
+                <div class="quote">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+                </div>
+            </div>
+            <div class="col-lg-4 wow animated fadeInLeft" data-wow-duration="1000ms" data-wow-delay="0.1s">
+                <div class="quote-btn">
+                    <a href="#" class="btn btn-success btn-lg">Request a quote</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content-wrapper section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include_once('footer.php');
