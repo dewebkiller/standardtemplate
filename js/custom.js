a6d94f041619e0868f8477fe891d098492bcd9d6
Niresh Shrestha <37860912+dewebkiller@users.noreply.github.com>
2022-12-21 12:07:18 +0545

    Update custom.js
    loader issue fixed.

--- js/custom.js
+++ js/custom.js
@@ -46,29 +46,10 @@ $(window).load(function () {
     wow.init();
 });
 
-// Repeat demo content
-var $body = $('body');
-var $box = $('.box');
-for (var i = 0; i < 20; i++) {
-    $box.clone().appendTo($body);
-}
-
-// Helper function for add element box list in WOW
-WOW.prototype.addBox = function (element) {
-    this.boxes.push(element);
-};
 // Init WOW.js and get instance
 var wow = new WOW();
 wow.init();
-// Attach scrollSpy to .wow elements for detect view exit events,
-// then reset elements and add again for animation
-$('.wow').on('scrollSpy:exit', function () {
-    $(this).css({
-        'visibility': 'hidden',
-        'animation-name': 'none'
-    }).removeClass('animated');
-    wow.addBox(this);
-}).scrollSpy();
+
 // Banner
 $('.owl-carousel').owlCarousel({
     loop: true,
